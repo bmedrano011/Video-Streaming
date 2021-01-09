de268268fcec92a5139325b09f5adfee4c8b42ce
B <bmedrano.ru@gmail.com>
2021-01-08 19:15:00 -0500

    Finalize player

--- src/Player.js
+++ src/Player.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import Header from './Header';
+import Footer from './Footer';
+
 export default class Player extends Component {
     constructor(props) {
         super(props);
@@ -9,9 +12,11 @@ export default class Player extends Component {
     }
     async componentDidMount() {
         try {
-            const res = await fetch(`http://localhost:4000/video/${this.state.videoId}/data`);
+            const res = await fetch(`http://localhost:4000/video/${
+                this.state.videoId
+            }/data`);
             const data = await res.json();
-            this.setState({ videoData: data });
+            this.setState({videoData: data});
         } catch (error) {
             console.log(error);
         }
@@ -19,12 +24,29 @@ export default class Player extends Component {
     render() {
         return (
             <div className="App">
+                <Header />
                 <header className="App-header">
-                    <video controls muted autoPlay>
-                        <source src={`http://localhost:4000/video/${this.state.videoId}`} type="video/mp4"></source>
+                
+                    <video controls muted autoPlay crossOrigin="anonymous">
+                        <source src={
+                                `http://localhost:4000/video/${
+                                    this.state.videoId
+                                }`
+                            }
+                            type="video/mp4"></source>
+                        <track label="English" kind="captions" srcLang="en"
+                            src={
+                                `http://localhost:4000/video/${
+                                    this.state.videoId
+                                }/caption`
+                            }
+                            default></track>
                     </video>
-                    <h1>{ this.state.videoData.name }</h1>
+                    <h1>{
+                        this.state.videoData.name
+                    }</h1>
                 </header>
+                <Footer />
             </div>
         )
     }
